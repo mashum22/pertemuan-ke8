98e7dbf1103e91c78b23f0b0574b8c66b2e926d3
mashum22 <116756625+mashum22@users.noreply.github.com>
2023-12-04 02:03:11 +0700

    update

---
+++ sekolah/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SI_SEKOLAH</title>
+</head>
+<body>
+    <h2>DATA SISWA UNIPDU MA`SHUM</h2>
+    <a href = "datasiswa.php">pendaftaran</a><br>
+    <a href = "tambahsiswa.php">data siswa</a>
+</body>
+</html>
